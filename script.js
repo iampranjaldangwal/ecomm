07265e7dc8fe0987cc513c2e569ce38d2594f3bb
milikotnala <kotnalamili1.com>
2025-04-05 16:25:55 +0530

    made separate css for men.html and women.html

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// Function to add navbar to pages
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("navbar.html")
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("navbar-container").innerHTML = data;
-
-        // Attach event listeners after navbar is loaded
-        document.getElementById("open-menu").addEventListener("click", openSidebar);
-        document.getElementById("close-menu").addEventListener("click", closeSidebar);
-      })
-      .catch(error => console.error("Error loading navbar:", error));
-});
-
-function openSidebar() {
-    document.getElementById("sidebar").style.width = "250px";
-}
-
-function closeSidebar() {
-    document.getElementById("sidebar").style.width = "0";
-}
-
 
 // Cart object to store product quantities
 let cart = {};
